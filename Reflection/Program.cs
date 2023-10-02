4c6e11bd464f3d8bee0766adfe13394a229754fa
RuslanSemchuk <134958046+RuslanSemchuk@users.noreply.github.com>
2023-10-02 23:39:35 +0300

    Add program cs

---
+++ Reflection/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+class MyClass
+{
+    public void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass obj = new MyClass();
+
+        
+        string methodName;
+        if (args.Length > 0)
+        {
+            methodName = args[0];
+        }
+        else
+        {
+            Console.Write("Enter the name of the method to call: ");
+            methodName = Console.ReadLine();
+        }
+
+        try
+        {
+            
+            MethodInfo method = typeof(MyClass).GetMethod(methodName);
+            if (method != null)
+            {
+                method.Invoke(obj, new object[] { "Hello World" });
+            }
+            else
+            {
+                Console.WriteLine("Method not found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
